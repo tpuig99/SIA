8885f7e755b71728d146fc928f9058f7e06095f5
Tamara <tamypuig@gmail.com>
2020-08-15 21:52:51 -0300

    feat: implemented H3 --> Manhattan + Distance Player-bag

---
+++ TP1/src/searchMethods/H3.java
@@ -0,0 +1,36 @@
+package searchMethods;
+
+import game.GameState;
+
+import java.awt.*;
+
+public class H3 implements Heuristic {
+    @Override
+    public int eval(GameState gameState) {
+        int value = 0;
+        int shortestGoal = Integer.MAX_VALUE;
+        int shortestPlayer = Integer.MAX_VALUE;
+        for (Point bag : gameState.getBags()) {
+            if (!gameState.isGoalCell((int) bag.getX(), (int) bag.getY())) {
+                for (Point goal : gameState.getGoals()) {
+                    shortestGoal = Math.min(shortestGoal, getDistance(bag, goal));
+                }
+                shortestPlayer = Math.min(shortestPlayer, getDistance(bag, gameState.getPlayer()));
+                value += shortestGoal;
+            }
+        }
+
+        return value + shortestPlayer;
+    }
+
+    public int getDistance (Point point1, Point point2) {
+        int x = Math.abs((int) point1.getX() - (int) point2.getX());
+        int y = Math.abs((int) point1.getY() - (int) point2.getY());
+        return x + y;
+    }
+
+    @Override
+    public String getName() {
+        return "H3";
+    }
+}
