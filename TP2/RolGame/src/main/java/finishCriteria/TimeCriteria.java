f223463d9f7c4d4cd018b164f11b37bd049f4fb9
Lboccardi <lboccardi@itba.edu.ar>
2020-08-31 18:53:45 -0300

    Time Criteria and Content Criteria completed.

--- TP2/RolGame/src/main/java/finishCriteria/TimeCriteria.java
+++ TP2/RolGame/src/main/java/finishCriteria/TimeCriteria.java
@@ -1,4 +1,23 @@
 package finishCriteria;
 
-public class TimeCriteria {
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import subjectModels.roles.Character;
+
+public class TimeCriteria implements FinishCriteria {
+    Long startTimeMillis = null;
+    Long durationMillis;
+
+    public TimeCriteria(long seconds) {
+        durationMillis = TimeUnit.SECONDS.toMillis(seconds);
+    }
+
+    @Override
+    public boolean shouldFinish(List<Character> population) {
+        if(startTimeMillis == null) {
+            startTimeMillis = System.currentTimeMillis();
+        }
+
+        return (System.currentTimeMillis() - startTimeMillis) >= durationMillis;
+    }
 }
