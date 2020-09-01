867ecc3c39d575481ecf5c1b81202516d2f632ef
Lboccardi <lboccardi@itba.edu.ar>
2020-09-01 00:54:13 -0300

    FillParent Implementation completed.

--- TP2/RolGame/src/main/java/implementations/FillParentImplementation.java
+++ TP2/RolGame/src/main/java/implementations/FillParentImplementation.java
@@ -1,4 +1,41 @@
 package implementations;
 
-public class FillParentImplementation {
+import java.util.ArrayList;
+import java.util.List;
+import subjectModels.roles.Character;
+
+
+public class FillParentImplementation implements Implementation {
+    @Override
+    public List<Character> evolve(List<Character> oldSubjects, List<Character> newSubjects, int K) {
+        List<Character> newGenerationList = new ArrayList<>();
+
+        int n = oldSubjects.size();
+
+        if (K < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        int i = 0;
+
+        if (K > n) {
+            while (newGenerationList.size() < n) {
+                newGenerationList.add(newSubjects.get(i%n));
+                i++;
+            }
+        }
+        else {
+            while (newGenerationList.size() < n) {
+                if (i < K) {
+                    newGenerationList.add(newSubjects.get(i));
+                }
+                else {
+                    newGenerationList.add(oldSubjects.get(i));
+                }
+                i++;
+            }
+        }
+
+        return newGenerationList;
+    }
 }
