beb83f9c254fd96fdec2ce8b9cbaa30b06187fed
Lboccardi <lboccardi@itba.edu.ar>
2020-09-15 01:53:43 -0300

    Finished EJ1. Results: AND can learn. XOR never learns.

--- TP3/Perceptron/src/main/java/Main.java
+++ TP3/Perceptron/src/main/java/Main.java
@@ -1,5 +1,36 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        ej1();
+    }
+
+    public static void ej1 () {
+        // AND
+        SimplePerceptron simplePerceptron = new SimplePerceptron(2);
+        double[][] input = {{-1, 1}, {1, -1}, {-1, -1}, {1, 1}};
+        double[] expected_output_AND = {-1, -1, -1, 1};
+        int i = 0;
+        int fails = 0;
+        while (i++ < 100) {
+            if (!Arrays.equals(simplePerceptron.train(input, expected_output_AND, 0.2, 100), expected_output_AND)) {
+             fails ++;
+            }
+        }
+        System.out.printf("There were %d fails\n", fails);
+
+        // XOR
+        SimplePerceptron simplePerceptron2 = new SimplePerceptron(2);
+        double[] expectedOutputXOR = {1, 1, -1, -1};
+        int XORi = 0;
+        int XORfails = 0;
+        while (XORi++ < 100) {
+            if (!Arrays.equals(simplePerceptron2.train(input, expectedOutputXOR, 0.2, 100), expectedOutputXOR)) {
+                XORfails ++;
+            }
+        }
+        System.out.printf("There were %d fails\n", XORfails);
+
+
     }
 }
---
+++ TP3/Perceptron/src/main/java/SimplePerceptron.java
@@ -0,0 +1,71 @@
+import java.util.Random;
+
+public class SimplePerceptron {
+    private int connections;
+    private double threshold;
+    private double[] w;
+    private final int ACTIVATE = 1, DEACTIVATE = -1;
+
+    public SimplePerceptron(int connections) {
+        this.connections = connections;
+        this.threshold = 0.0;
+        this.w = new double[connections];
+        for (int i = 0; i < connections; i++) {
+            w[i] = Math.random() * 2 - 1;
+        }
+    }
+
+    public int sign (double [] excitations) {
+        double stimuli = 0;
+        for (int i = 0; i < w.length ; i++) {
+            stimuli += w[i]*excitations[i];
+        }
+        return stimuli > threshold ? ACTIVATE : DEACTIVATE;
+    }
+
+    public double[] train (double[][] input, double[] expected_output, double learning_rate, int steps ) {
+        Random rnd = new Random();
+        double error = 1;
+        double error_min = 2*expected_output.length;
+        double[] w_min = new double[w.length];
+        int i = 0;
+        while (error > 0 && i < steps) {
+            if ( i < steps * learning_rate) {
+                for (int j = 0; j < connections; j++) {
+                    w[j] = Math.random() * 2 - 1;
+                }
+            }
+            int curr = rnd.nextInt(expected_output.length);
+            double output = sign(input[curr]);
+
+            threshold += learning_rate * (expected_output[curr] - output) * (-1);
+
+            for (int j = 0; j < this.connections; j++) {
+                w[j] += learning_rate * (expected_output[curr] - output) * input[curr][j];
+            }
+
+            error = calculateError(input, expected_output);
+            if (error < error_min) {
+                error_min = error;
+                w_min = w.clone();
+            }
+            i++;
+        }
+        double [] toReturn = new double [input.length];
+        for (int k = 0; k < input.length; k++) {
+            toReturn[k] = sign(input[k]);
+        }
+
+        return toReturn;
+    }
+
+    private double calculateError (double[][] input, double[] expected_output) {
+        double error = 0;
+        for (int i = 0; i < input.length; i++) {
+            if (sign(input[i]) != (int) expected_output[i]) {
+                error++;
+            }
+        }
+        return error/input.length;
+    }
+}
