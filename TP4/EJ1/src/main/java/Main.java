69824f0caeccba1444706a1a71720c6455b9dd72
Lboccardi <lboccardi@itba.edu.ar>
2020-10-21 16:33:36 -0300

    Added CSV generation for graphics

--- TP4/EJ1/src/main/java/Main.java
+++ TP4/EJ1/src/main/java/Main.java
@@ -2,6 +2,7 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.lang3.ArrayUtils;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -14,31 +15,70 @@ import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // KohonenNetworkExercise();
+         KohonenNetworkExercise();
          OjaRuleExercise();
     }
 
-    public static void KohonenNetworkExercise() {
+    public static void KohonenNetworkExercise() throws IOException {
+
+        double eta = 0.1;
+        int epochs = 100;
+        double initialNeighbourhoodSize = 3;
+        int matrix_x_dim = 3;
+        int matrix_y_dim = 3;
+        int[] grid = new int[] {matrix_x_dim, matrix_y_dim};
+
+        System.out.println("############################################################");
+        System.out.println("############################################################");
+        System.out.println("Executing Kohonen Exercise with the following parameters:");
+        System.out.println("Eta: " + eta);
+        System.out.println("Epochs: " + epochs);
+        System.out.println("Initial Neighbour Size " + initialNeighbourhoodSize);
+        System.out.println("Epochs: " + epochs);
+        System.out.println("############################################################");
+        System.out.println("############################################################\n");
+
         double[][] parsedData = getNormalizedValues();
+        String[] parsedCountries = getCountriesValues();
 
-        KohonenNetwork kohonenNetwork = new KohonenNetwork(parsedData[0].length, new int[]{3, 3});
+        KohonenNetwork kohonenNetwork = new KohonenNetwork(parsedData[0].length, grid);
 
-        kohonenNetwork.train(parsedData, 0.1, 100, 3);
-        int i = 0;
+        kohonenNetwork.train(parsedData, eta, epochs, initialNeighbourhoodSize);
+
+        BufferedWriter writer1 = Files.newBufferedWriter(Paths.get("./distance_matrix.csv"), StandardCharsets.UTF_8);
+        CSVPrinter csvPrinter1 = new CSVPrinter(writer1, CSVFormat.DEFAULT);
+        System.out.println("Final distance matrix:");
         for (double[] row : kohonenNetwork.getDistanceMatrix()) {
-            System.out.println("Row "+(i++)+": "+ Arrays.toString(row));
+            Double[] doubleArray = ArrayUtils.toObject(row);
+            System.out.println(Arrays.toString(row));
+            csvPrinter1.printRecord(Arrays.asList(doubleArray));
         }
+        System.out.println();
+        csvPrinter1.close();
 
+        BufferedWriter writer2 = Files.newBufferedWriter(Paths.get("./countries_and_neurons.csv"), StandardCharsets.UTF_8);
+        CSVPrinter csvPrinter2 = new CSVPrinter(writer2, CSVFormat.DEFAULT.withHeader("country", "neuron"));
+        System.out.println("Countries and Associated Neurons:");
         for (int country = 0; country < parsedData.length; country++) {
             Integer[] associatedNeuron = kohonenNetwork.getWinningNeuronCoordinates(parsedData[country]);
-            System.out.printf("Country %d: Associated Neuron: (%d, %d)\n", country+1 , associatedNeuron[0], associatedNeuron[1]);
+            System.out.printf("Country %s: Associated Neuron: (%d, %d)\n", parsedCountries[country] , associatedNeuron[0], associatedNeuron[1]);
+            String neuron = String.format("(%d;%d)", associatedNeuron[0], associatedNeuron[1]);
+            csvPrinter2.printRecord(parsedCountries[country], neuron);
         }
+        System.out.println();
+        csvPrinter2.close();
 
-        i = 0;
+        BufferedWriter writer3 = Files.newBufferedWriter(Paths.get("./registers_matrix.csv"), StandardCharsets.UTF_8);
+        CSVPrinter csvPrinter3 = new CSVPrinter(writer3, CSVFormat.DEFAULT);
+        System.out.println("Registers by Neuron:");
         for (int[] row : kohonenNetwork.getRegistersMatrix(parsedData)) {
-            System.out.println("Row "+(i++)+": "+ Arrays.toString(row));
+            Integer[] integerArray = ArrayUtils.toObject(row);
+            System.out.println(Arrays.toString(row));
+            csvPrinter3.printRecord(Arrays.asList(integerArray));
         }
+        csvPrinter3.close();
 
+        System.out.println("\n\n");
     }
 
     public static void OjaRuleExercise() throws IOException {
@@ -57,7 +97,7 @@ public class Main {
 
         OjaRulePerceptron ojaRulePerceptron = new OjaRulePerceptron(parsedData[0].length);
 
-        ojaRulePerceptron.train(parsedData, 0.4, 100000);
+        ojaRulePerceptron.train(parsedData, eta, epochs);
 
         BufferedWriter writer = Files.newBufferedWriter(Paths.get("./oja_rule.csv"), StandardCharsets.UTF_8);
         CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader("country", "index"));
