b6fe1a36cc0044748966f09d5a9c53aa1b347a0c
Lboccardi <lboccardi@itba.edu.ar>
2020-11-07 10:36:42 -0300

    Initialized TP5

---
+++ TP5/Autoencoder/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 0; i < Constants.font1.length ; i++) {
+            System.out.println(Arrays.toString(Constants.font1[i]));
+        }
+    }
+}
